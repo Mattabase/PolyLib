64890b68a5d6be4c713f654ec953e9365ecbf8b6
Gigabit101 <thegigabit101@gmail.com>
2026-04-24 16:45:32 +0100

    Add test for blockentity client packet

--- testmod-common/src/main/java/net/creeperhost/testmod/blocks/inventorytestblock/BlockEntityInventoryTest.java
+++ testmod-common/src/main/java/net/creeperhost/testmod/blocks/inventorytestblock/BlockEntityInventoryTest.java
@@ -6,13 +6,22 @@ import net.creeperhost.polylib.data.serializable.StringData;
 import net.creeperhost.polylib.data.serializable.UUIDData;
 import net.creeperhost.polylib.inventory.items.BlockInventory;
 import net.creeperhost.polylib.inventory.items.PolyInventoryBlock;
+import net.creeperhost.polylib.inventory.power.IPolyEnergyStorage;
+import net.creeperhost.polylib.inventory.power.PolyBlockEnergyStorage;
+import net.creeperhost.polylib.inventory.power.PolyEnergyBlock;
+import net.creeperhost.polylib.inventory.power.PolyEnergyStorage;
+import net.creeperhost.testmod.TestModCommon;
 import net.creeperhost.testmod.init.TestBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.cow.Cow;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -21,10 +30,11 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.Nullable;
 
-public class BlockEntityInventoryTest extends PolyBlockEntity implements PolyInventoryBlock, MenuProvider
+public class BlockEntityInventoryTest extends PolyBlockEntity implements PolyInventoryBlock, PolyEnergyBlock, MenuProvider
 {
     /**
      * A user-editable label stored in NBT and synced to clients.
@@ -40,6 +50,8 @@ public class BlockEntityInventoryTest extends PolyBlockEntity implements PolyInv
 
     public final BlockInventory simpleItemInventory = new BlockInventory(this, 1);
     public final BlockInventory outputInv = new BlockInventory(this, 1);
+    public final PolyEnergyStorage energyContainer = new PolyBlockEnergyStorage(this, 1000000);
+
     int progress = 0;
     public IntData testSyncedIntField = register("test_int", new IntData(0), SAVE, SYNC, CLIENT_CONTROL);
 
@@ -104,5 +116,18 @@ public class BlockEntityInventoryTest extends PolyBlockEntity implements PolyInv
     public void readExtraData(ValueInput input) {
         simpleItemInventory.deserialize(input);
         outputInv.deserialize(input.childOrEmpty("out_inv"));
+    }
+
+    @Override
+    public IPolyEnergyStorage getEnergyStorage(@org.jetbrains.annotations.Nullable Direction side) {
+        return energyContainer;
+    }
+
+    @Override
+    public void handlePacketFromClient(ServerPlayer player, int id, FriendlyByteBuf buf) {
+        TestModCommon.LOGGER.info("Message from client! {}", player);
+        Cow cow = new Cow(EntityType.COW, level);
+        cow.setPos(Vec3.atBottomCenterOf(getBlockPos().above()));
+        level.addFreshEntity(cow);
     }
 }
